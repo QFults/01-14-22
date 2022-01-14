c946a43bd977415e517316139d5334169564d498
Quinton Fults <quinton.fults.work@gmail.com>
2022-01-14 13:12:37 -0800

    code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import Navbar from './components/Navbar'
 
+const dog = {
+  name: 'Beef',
+  age: 2,
+  breed: 'Chihuahua'
+}
+
+const title = 'My Awesome App'
+
 const App = () => {
   return (
     <>
-      <Navbar />
+      <Navbar title={title} />
       <h1>Hello World!</h1>
       <button className="btn btn-primary">Click Me</button>
+      <p>Name: {dog.name}</p>
+      <p>Age: {dog.age}</p>
+      <p>Breed: {dog.breed}</p>
     </>
   )
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,9 @@
-const Navbar = () => {
+const Navbar = props => {
+  console.log(props)
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+        <a className="navbar-brand" href="#">{props.title}</a>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -26,7 +27,7 @@ const Navbar = () => {
               </ul>
             </li>
             <li className="nav-item">
-              <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              <a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
             </li>
           </ul>
           <form className="d-flex">
